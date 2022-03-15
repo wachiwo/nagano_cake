91f35263773e777f6737addbd457c3530e2e010e
EC2 Default User <ec2-user@ip-172-31-10-240.ec2.internal>
2022-03-15 16:51:23 +0000

    customers migrate

--- config/routes.rb
+++ config/routes.rb
@@ -2,72 +2,72 @@ Rails.application.routes.draw do
   namespace :admin do
     get 'homes/top'
   end
-  
+
   namespace :admin do
-    resources :sessions,only: [new,create,destroy]
+    resources :sessions,only: [:new,:create,:destroy]
   end
 
   namespace :admin do
-    resources :items,except: [destroy]
+    resources :items,except: [:destroy]
   end
-  
-  namespace :admin do 
-    resources :genres,only: [index,create,edit,update]
-  end
-  
+
   namespace :admin do
-    resources :customers,only: [index,show,edit,update]
+    resources :genres,only: [:index,:create,:edit,:update]
   end
-  
+
   namespace :admin do
-    resources :orders,only[show,update]
+    resources :customers,only: [:index,:show,:edit,:update]
   end
-  
+
   namespace :admin do
-    resources :order_details,only[update]
+    resources :orders,only: [:show,:update]
   end
-      
+
+  namespace :admin do
+    resources :order_details,only: [:update]
+  end
+
   namespace :public do
     get 'homes/top'
     get 'homes/about'
   end
-      
+
   namespace :public do
-    resources :orders,only: [new,create,index,show]
+    resources :orders,only: [:new,:create,:index,:show]
     get 'orders/comfirm'
     get 'orders/complete'
   end
-  
+
   namespace :public do
-    resources :cart_items,only: [index,update,destroy,create]
+    resources :cart_items,only: [:index,:update,:destroy,:create]
     get 'cart_items/destroy_all'
   end
-  
+
   namespace :public do
-    resources :customers,only: [show,edit,update]
+    resources :customers,only: [:show,:edit,:update]
     get 'customers/unsubscibe'
     get 'customers/withdraw'
     patch 'customers/withdraw'
   end
-  
+
   namespace :public do
     get 'items/index'
     get 'items/show'
   end
-  
+
   namespace :public do
-   resources :registrations,only: [get,post]
+   resources :registrations,only: [:get,:post]
   end
-  
+
   namespace :public do
-    resources :sessions,only: [new,create,destroy]
+    resources :sessions,only: [:new,:create,:destroy]
   end
-    
+
   namespace :public do
-    resources :addresses,only: [index,edit,create,update,destroy]
+    resources :addresses,only: [:index,:edit,:create,:update,:destroy]
   end
-  
-    
-  
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ db/schema.rb
@@ -0,0 +1,30 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2022_03_15_152914) do
+
+  create_table "customers", force: :cascade do |t|
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "last_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.string "email", null: false
+    t.string "encrypted_password", null: false
+    t.string "postal_code", null: false
+    t.string "address", null: false
+    t.string "telephone_number", null: false
+    t.boolean "is_deleted", default: false, null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
