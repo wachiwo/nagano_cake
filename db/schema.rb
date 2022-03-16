d2755b53b116e10f00558b90a5132b64491ea1a1
EC2 Default User <ec2-user@ip-172-31-10-240.ec2.internal>
2022-03-16 17:59:25 +0000

    全体入力

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_15_173039) do
+ActiveRecord::Schema.define(version: 2022_03_16_134953) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customers_id", null: false
@@ -20,6 +20,18 @@ ActiveRecord::Schema.define(version: 2022_03_15_173039) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["customers_id"], name: "index_addresses_on_customers_id"
+  end
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
   create_table "cart_items", force: :cascade do |t|
@@ -33,18 +45,21 @@ ActiveRecord::Schema.define(version: 2022_03_15_173039) do
   end
 
   create_table "customers", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.string "last_name", null: false
     t.string "first_name", null: false
     t.string "last_name_kana", null: false
     t.string "first_name_kana", null: false
-    t.string "email", null: false
-    t.string "encrypted_password", null: false
     t.string "postal_code", null: false
     t.string "address", null: false
     t.string "telephone_number", null: false
-    t.boolean "is_deleted", default: false, null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.boolean "is_deleted", null: false
+    t.index ["email"], name: "index_customers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
   create_table "genres", force: :cascade do |t|
